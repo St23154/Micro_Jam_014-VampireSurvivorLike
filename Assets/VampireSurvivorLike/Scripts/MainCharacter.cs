7d18178f135497c3e5299cb91404eed48cdf09c7
St23154 <e2peyrot@gmail.com>
2024-05-18 14:47:17 +0200

    Player + Fleche qui marchent

--- Assets/VampireSurvivorLike/Scripts/MainCharacter.cs
+++ Assets/VampireSurvivorLike/Scripts/MainCharacter.cs
@@ -7,7 +7,9 @@ public class MainCharacter : MonoBehaviour
 {
 
     public float _speed = 2f;
-    public Vector3 _target;
+    public GameObject _arrows;
+    private Vector3 _target;
+    private GameObject _arrowsToDelete;
     public Camera _mainCamera;
     private bool _isMolletteButtonDown = false;
 
@@ -32,13 +34,22 @@ public class MainCharacter : MonoBehaviour
 
         //Move
         
-
         if (_isMolletteButtonDown)
         {
+            _arrowsToDelete = GameObject.FindGameObjectWithTag("ArrowPointer");
             _target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             _target.z = transform.position.z;
+            Destroy(_arrowsToDelete);
+            Instantiate(_arrows, _target, transform.rotation);
+            _isMolletteButtonDown = false;
         }
 
         transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+        
+        if (transform.position == _target)
+        {
+            _arrowsToDelete = GameObject.FindGameObjectWithTag("ArrowPointer");
+            Destroy(_arrowsToDelete);
+        }
     }
 }
