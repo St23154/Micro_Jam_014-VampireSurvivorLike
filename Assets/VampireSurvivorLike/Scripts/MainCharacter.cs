2771edafec0777a724ee83ae39a44b58595d25e8
St23154 <e2peyrot@gmail.com>
2024-05-18 20:20:14 +0200

    player animation controller and

--- Assets/VampireSurvivorLike/Scripts/MainCharacter.cs
+++ Assets/VampireSurvivorLike/Scripts/MainCharacter.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEditor.Timeline;
 using UnityEngine;
 
@@ -11,9 +12,15 @@ public class MainCharacter : MonoBehaviour
     private Vector3 _target;
     private GameObject _arrowsToDelete;
     public Camera _mainCamera;
+    private Animator _animator;
     private bool _isMolletteButtonDown = false;
     private bool _stop = false;
 
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -26,9 +33,9 @@ public class MainCharacter : MonoBehaviour
         {
             _isMolletteButtonDown = false;
         }
-
-        //Move
         
+        //Display arrows and set target where the player click
+
         if (_isMolletteButtonDown)
         {
             _arrowsToDelete = GameObject.FindGameObjectWithTag("ArrowPointer");
@@ -37,9 +44,15 @@ public class MainCharacter : MonoBehaviour
             Destroy(_arrowsToDelete);
             Instantiate(_arrows, _target, transform.rotation);
             _isMolletteButtonDown = false;
+            RotatePlayerAnimation(_target.x - transform.position.x, _target.y - transform.position.y);
+            
         }
 
+        //Move
+
         transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+
+        //Destroy arrows if player is on
         
         if (transform.position == _target)
         {
@@ -47,4 +60,44 @@ public class MainCharacter : MonoBehaviour
             Destroy(_arrowsToDelete);
         }
     }
+
+    private void RotatePlayerAnimation(float Xpos, float Ypos)
+    {
+        if ((Xpos != 0) || (Ypos != 0))
+            {
+                if (Xpos < 0)
+                {
+                    if (Xpos * - 1 > math.abs(Ypos))
+                    {
+                        _animator.SetFloat("X", -1);
+                        _animator.SetFloat("Y", 0);
+                    }
+                }
+                if (Ypos < 0)
+                {
+                    if (Ypos * - 1 > math.abs(Xpos))
+                    {
+                        _animator.SetFloat("Y", -1);
+                        _animator.SetFloat("X", 0);
+                    }
+                }
+                if (Xpos > 0)
+                {
+                    if (Xpos > math.abs(Ypos) )
+                    {
+                        _animator.SetFloat("X", 1);
+                        _animator.SetFloat("Y", 0);
+                    }
+                }
+                if (Ypos > 0)
+                {
+                    if (Ypos > math.abs(Xpos) )
+                    {
+                        _animator.SetFloat("Y", 1);
+                        _animator.SetFloat("X", 0);
+                    }
+                }
+            }
+    }
+
 }
