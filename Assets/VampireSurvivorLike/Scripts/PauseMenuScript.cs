e8cd26f63fa18d7b2040121bd9a4da7a36068e2e
EmilienB <emiliendollar@gmail.com>
2024-05-18 20:20:35 +0200

    Menu pause pas bo

---
+++ Assets/VampireSurvivorLike/Scripts/PauseMenuScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    // Start is called before the first frame update
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale=0.2f;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale=1;
+    }
+
+}
